07ad683527e581a73703e8b2c69a07d7fe34ac45
Mishan20 <ishansenanayaka1@gmail.com>
2024-01-08 17:17:00 +0530

    edit pages

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/detail.dart';
-import 'package:frontend/profile.dart';
-import 'package:frontend/search.dart';
+// import 'package:frontend/detail.dart';
+// import 'package:frontend/profile.dart';
+// import 'package:frontend/search.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
